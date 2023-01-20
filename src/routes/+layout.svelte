2e25a8fd97a9b4cee21484dfc98922885d308a27
dompetriella <dompetriella@gmail.com>
2023-01-19 22:46:38 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script>
+	// import './styles.css';
+</script>
+
+<div class="app">
+	<nav>
+	</nav>
+
+	<main>
+		<slot />
+	</main>
+
+	<!-- <footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer> -->
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+	}
+
+	@media (min-width: 480px) {
+        
+	}
+</style>
