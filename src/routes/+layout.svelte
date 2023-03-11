7b8eca02517edfe3b870dd82900183a8957f5ff1
dompetriella <dompetriella@gmail.com>
2023-03-11 15:55:13 -0500

    add base of navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
 	import './styles.css';
 	import '../app.css'
+
 </script>
 
 <div class="app">
 	<nav>
+		<div class="navbar-items">
+		</div>
 	</nav>
 	
 	<main>
@@ -21,10 +24,32 @@
 		padding: 0px;
 	}
 
+	nav {
+		position: fixed;
+		bottom: 1em;
+		display: flex;
+		align-items: center;
+		align-self: center;
+		width: 85vw;
+		height: 3em;
+		background-color: var(--lightModeSecondary);
+		border-radius: 15px;
+		box-shadow: -5px 5px 0em 2px var(--darkGreenText);
+		z-index: 999;
+	}
+
+	.navbar-items {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+
 	main {
 		width: 100vw;
 		height: 100vh;
 		background-color: var(--lightModePrimary);
+
 	}
 
 	@media (min-width: 480px) {
