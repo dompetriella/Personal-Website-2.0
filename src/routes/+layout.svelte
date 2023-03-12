d9441bcca513e6596242cbee125d315b6f3b15dd
dompetriella <dompetriella@gmail.com>
2023-03-12 12:36:23 -0400

    desktop navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -45,12 +45,45 @@
 		/>
 	</nav>
 
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<img
+		class="background-toggle"
+		on:click={toggleLightMode}
+		src={$lightMode ? '/sun.png' : '/fullmoon.png'}
+		alt={$lightMode ? 'sun' : 'moon'}
+		height="350"
+		width="350"
+	/>
 	<nav
 		class="desktop"
 		style={$lightMode
 			? 'background-color: var(--lightModeSecondary); box-shadow: -5px 5px 0em 2px var(--darkGreenText);'
 			: 'background-color: var(--darkModeSecondary); box-shadow: -5px 5px 0em 2px var(--darkText);'}
-	/>
+	>
+		<div class="left-nav">
+			<div
+				class="nav-text"
+				style="color: {$lightMode ? 'var(--darkGreenText)' : 'var(--lightText)'}"
+			>
+				HOME
+			</div>
+		</div>
+
+		<div class="right-nav">
+			<div
+				class="nav-text"
+				style="color: {$lightMode ? 'var(--darkGreenText)' : 'var(--lightText)'}"
+			>
+				PROJECTS
+			</div>
+			<div
+				class="nav-text"
+				style="color: {$lightMode ? 'var(--darkGreenText)' : 'var(--lightText)'}"
+			>
+				CONTACT
+			</div>
+		</div>
+	</nav>
 
 	<main
 		style={$lightMode
@@ -69,27 +102,37 @@
 		padding: 0px;
 	}
 
-	nav.mobile {
-		position: fixed;
-		bottom: 1em;
-		display: flex;
-		align-items: center;
-		align-self: center;
-		justify-content: space-around;
-		width: 85vw;
-		height: 3em;
-		border-radius: 15px;
-		z-index: 999;
-	}
-
-	.icon-image {
-		padding-left: 1em;
-		animation: fadeIn 1s;
-	}
-
 	main {
 		width: 100vw;
 		height: 100vh;
+	}
+
+	@media (max-width: 480px) {
+		nav.mobile {
+			position: fixed;
+			bottom: 1em;
+			display: flex;
+			align-items: center;
+			align-self: center;
+			justify-content: space-around;
+			width: 85vw;
+			height: 3em;
+			border-radius: 15px;
+			z-index: 999;
+		}
+
+		.icon-image {
+			padding-left: 1em;
+			animation: fadeIn 1s;
+		}
+
+		.background-toggle {
+			display: none;
+		}
+
+		.nav-text {
+			display: none;
+		}
 	}
 
 	@media (min-width: 480px) {
@@ -103,10 +146,37 @@
 			display: flex;
 			align-items: center;
 			align-self: center;
+			justify-content: space-between;
 			width: 90vw;
 			height: 3em;
 			border-radius: 15px;
 			z-index: 999;
+		}
+
+		.nav-text {
+			font-size: 1.2em;
+			font-weight: 100;
+			letter-spacing: 1px;
+			padding: 0em 1em;
+		}
+
+		.left-nav {
+			display: flex;
+			flex-direction: row;
+			justify-content: flex-start;
+		}
+
+		.right-nav {
+			display: flex;
+			flex-direction: row;
+			justify-content: flex-end;
+		}
+
+		.background-toggle {
+			display: block;
+			position: fixed;
+			top: -9em;
+			left: -8em;
 		}
 	}
 </style>
