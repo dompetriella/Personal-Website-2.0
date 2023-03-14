8878d2b66cf451f9a041e9b56549968bf1aa283a
dompetriella <dompetriella@gmail.com>
2023-03-13 23:19:51 -0400

    started adding opening-screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,63 @@
-<script>
+<script lang="ts">
 	import HeaderSign from '../components/header-sign.svelte';
+	import { lightMode } from '../stores';
+	let screenSize: number;
+	const mobile: number = 480;
 </script>
 
-<div style="display: flex; align-items: center; padding-top: 30px; center; flex-direction: column">
-	<HeaderSign />
+<svelte:window bind:innerWidth={screenSize} />
+
+<div class="main-tree">
+	<div class="opening-screen">
+		<div class="header-area">
+			<HeaderSign fontSize={screenSize > mobile ? 5 : 2.7} />
+			<div style="color: {$lightMode ? 'var(--darkText)' : 'var(--lightText)'}" class="header-text">
+				<div>I'M DOM PETRIELLA</div>
+				<div>{$lightMode ? 'FULL STACK DEVELOPER' : 'VISUAL DESIGNER'}</div>
+				<div>{$lightMode ? 'BY DAY' : 'BY NIGHT'}</div>
+			</div>
+		</div>
+	</div>
 </div>
 
-<style></style>
+<style>
+	.main-tree {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100vh;
+	}
+
+	.header-text {
+		text-align: center;
+		letter-spacing: 2px;
+		padding: 1em;
+	}
+
+	.opening-screen {
+		display: flex;
+	}
+
+	/* mobile */
+	@media (max-width: 480px) {
+		.header-area {
+			padding-top: 2em;
+		}
+	}
+
+	/* desktop */
+	@media (min-width: 480px) {
+		.main-tree {
+			align-items: start;
+		}
+
+		.header-area {
+			padding-left: 10em;
+			padding-top: 15em;
+		}
+
+		.header-text {
+			font-size: 1.5em;
+		}
+	}
+</style>
