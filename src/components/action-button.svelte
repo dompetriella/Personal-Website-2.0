174178e003a6782df9afeb1bbc0a19f31302cbe5
dompetriella <dompetriella@gmail.com>
2023-04-27 22:25:48 -0400

    updated button

--- src/components/action-button.svelte
+++ src/components/action-button.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import { lightMode } from '../stores';
 	enum ButtonType {
 		Primary,
@@ -10,6 +11,8 @@
 	export let textSize: number = 1.5;
 	export let verticalPadding: number = 0.4;
 	export let horizontalPadding: number = 2;
+
+	let isPressed = false;
 
 	$: buttonAttributes = determineButtonAttributes($lightMode, buttonType);
 
@@ -48,12 +51,17 @@
 </script>
 
 <div
-	style="background-color: {buttonAttributes.bgColor}; 
-	box-shadow: -0.1em 0.15em 0 0.075em {buttonAttributes.shadowColor}; 
-	color: {buttonAttributes.textColor};  
-	font-size: {textSize}em; 
-	padding: {verticalPadding}em {horizontalPadding}em;"
 	class="action-button"
+	class:pressed={isPressed}
+	style={`background-color: ${buttonAttributes.bgColor};
+          color: ${buttonAttributes.textColor};
+          font-size: ${textSize}em;
+          padding: ${verticalPadding}em ${horizontalPadding}em;
+          box-shadow: ${isPressed ? '0px 3px 0px 2px' : '-3px 5px 0 0.075em'} ${
+		buttonAttributes.shadowColor
+	};`}
+	on:mousedown={() => (isPressed = true)}
+	on:mouseup={() => (isPressed = false)}
 >
 	{text}
 </div>
@@ -67,8 +75,15 @@
 		width: max-content;
 		cursor: pointer;
 		user-select: none;
+		transition: box-shadow 0.2s ease-out, transform 0.2s ease-out;
 	}
+
 	.action-button:hover {
 		filter: brightness(105%);
+	}
+
+	.action-button.pressed {
+		box-shadow: 1px 1px 1px 1px var(--lightModeHighlight);
+		transform: translateY(5px) scale(0.95);
 	}
 </style>
