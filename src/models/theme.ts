3cb8676cf3ee1d18473088916ddd458f58c88c05
Dominic Petriella <dompetriella@gmail.com>
2023-09-22 00:08:48 -0500

    added themes class

---
+++ src/models/theme.ts
@@ -0,0 +1,28 @@
+export class Theme {
+
+    static lightText: string = '#E8E4E4';
+    static dartText: string = '#171717';
+    static dartGreenText: string = '#2B452D';
+    static dartBlueText: string = '#385961';
+
+    static lightModePrimary: string = '#63A66A';
+    static lightModeSecondary: string = '#76C77E';
+    static lightModeHighlight: string = '#D0C777';
+
+    static lightModeSplashPrimary: string = '#639AA6';
+    static lightModeSplashSecondary: string = '#63A6A2';
+    static lightModeSplashTertiary: string = '#63A68E';
+
+
+    static darkModePrimary: string = '#1E1E1E';
+    static darkModeSecondary: string =  '#2B2B2C';
+    static darkModeHighlight: string = '#C8651D';
+    static darkModeHighlightSecondary: string =  'rgb(200, 101, 29, 0.25)';
+    
+    static darkModeSplashPrimary: string = '#E8E4E4';
+    static darkModeSplashSecondary: string =  '#606061';
+    static darkModeSplashTertiary: string = '#222222;'
+
+}
+
+
